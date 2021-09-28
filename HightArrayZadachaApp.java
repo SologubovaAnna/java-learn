329e1a304f298b4f7ded527681944abc936bd92f
Anna Sologubova <anett_vladimirovna@mail.ru>
2021-09-28 14:28:00 +0300

    Main класс для HightArrayZadacha с max элементом моссива или -1 если пуст

--- HightArrayZadachaApp.java
+++ HightArrayZadachaApp.java
@@ -6,4 +6,39 @@
 массив пуст
  *  Last modified:     September 14, 2021
  **************************************************************************** */
-class
+class HightArrayZadachaApp {
+    public static void main(String[] args) {
+        int max = 100;
+        HightArrayZadacha arr;
+        arr = new HightArrayZadacha(max);
+
+        arr.insert(5);
+        arr.insert(8);
+        arr.insert(77);
+        arr.insert(105);
+        arr.insert(57);
+        arr.insert(88);
+        arr.insert(99);
+        arr.insert(71);
+        arr.insert(22);
+        arr.insert(33);
+
+
+        arr.display();
+
+        arr.getMax();
+
+        arr.display();
+
+        arr.delete(105);
+
+        arr.display();
+
+        arr.getMax();
+
+        int s = 77;
+        arr.find(s);
+        
+
+    }
+}
