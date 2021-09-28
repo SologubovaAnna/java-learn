3f67d1c789c4801e4091100da4631fb974adb083
Anna Sologubova <anett_vladimirovna@mail.ru>
2021-09-28 21:35:53 +0300

    arrt - новый массив, в который вставляются отсортированные эл-ты от max до min

--- HightArrayZadachaApp.java
+++ HightArrayZadachaApp.java
@@ -3,14 +3,20 @@
  *  Coursera User ID:  1234562
  * Конструктор Класс массива с высокоуровневым интерфейсом
  * getMax(), который возвращает наибольшее значение ключа в массиве или –1, если
-массив пуст
+ * массив пуст
+ * arrt - новый массив, в который вставляются отсортированные эл-ты от max до min
+ * removeMax(); - отвечает за сортироку.
+ * т.е. в цикле for loop массива arr вставляем max эл. в массив arrt и удаляем эл из массива arr
+ * и так пока элементов в массиве arr не будет
+ * получается что отсортированные эл сохраняться в масссиве arrt
  *  Last modified:     September 14, 2021
  **************************************************************************** */
 class HightArrayZadachaApp {
     public static void main(String[] args) {
-        int max = 100;
-        HightArrayZadacha arr;
+        int max = 11;
+        HightArrayZadacha arr, arrt;
         arr = new HightArrayZadacha(max);
+
 
         arr.insert(5);
         arr.insert(8);
@@ -28,17 +34,34 @@ class HightArrayZadachaApp {
 
         arr.getMax();
 
-        arr.display();
+        //   arr.display();
 
-        arr.delete(105);
+        // arr.delete(5);
 
-        arr.display();
+        //arr.display();
 
-        arr.getMax();
+        //  arr.getMax();
 
         int s = 77;
         arr.find(s);
-        
+
+        //   arr.removeMax();
+        //  arr.display();
+        //arr.delete(99);
+        //  arr.getMax();
+        //   arr.display();
+
+
+        int m = 100;
+        arrt = new HightArrayZadacha(m);
+        for (int j = 0; j < max; j++) {
+
+            arrt.insert(arr.getMax());
+            arr.removeMax();
+
+        }
+        arrt.display();
+
 
     }
 }
