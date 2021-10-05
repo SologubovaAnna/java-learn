ddb2c0f55aa76bb4d29fed3407437b816e523156
anett_vladimirovna@mail.ru <anett_vladimirovna@mail.ru>
2021-10-05 17:12:47 +0500

    вставка случайных чисел в массив. Очень медленная сортировка. Если указать 10000 эл в моссиве то наглядно как долго происходит сортировка.

--- BubbleSortApp.java
+++ BubbleSortApp.java
@@ -7,9 +7,10 @@
 
 class BubbleSortApp {
     public static void main(String[] args) {
-        int maxSize = 100;
+        int maxSize = 100000;
         ArrayBub arr;
         arr = new ArrayBub(maxSize);
+
         arr.insert(22);
         arr.insert(33);
         arr.insert(88);
@@ -25,5 +26,29 @@ class BubbleSortApp {
         arr.bubbleSort();
         System.out.println("After sort:");
         arr.display();
+
+
+
+        /*
+//вставка случайных чисел в массив
+        for (int j = 0; j < maxSize; j++) // Заполнение массива
+        { // случайными числами
+            long n = (long) (java.lang.Math.random() * (maxSize - 1));
+            arr.insert(n);
+            //сортировка при вставки
+            arr.bubbleSort();
+        }
+
+
+        System.out.println("Before sort: ");
+        arr.display();
+
+        // arr.bubbleSort();
+        System.out.println("After sort:");
+        arr.display();
+
+
+
+         */
     }
 }
